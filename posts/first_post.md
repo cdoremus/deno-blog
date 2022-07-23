f2d1cd7d298e2721065102532c05568f5f5cc24e
Craig Doremus <cdoremus@gmail.com>
2022-07-22 21:26:00 -0400

    Initial commit

---
+++ posts/first_post.md
@@ -0,0 +1,8 @@
+
+# First Blog Post
+
+## Section 1
+This is section 1 of my first blog post.
+
+## Section 2
+This is section 2 of my first blog post.
---
+++ routes/index.tsx
@@ -0,0 +1,19 @@
+/** @jsx h */
+import { h } from "preact";
+import { tw } from "@twind";
+import Counter from "../islands/Counter.tsx";
+
+export default function Home() {
+  return (
+    <div class={tw`p-4 mx-auto max-w-screen-md`}>
+      <img
+        src="/logo.svg"
+        height="100px"
+        alt="the fresh logo: a sliced lemon dripping with juice"
+      />
+      <p>
+        <a href="/blog">Blog</a>
+      </p>
+    </div>
+  );
+}
