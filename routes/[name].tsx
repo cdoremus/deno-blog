9a1102bbba135c7c1e0af4985202681a3dbed907
Craig Doremus <cdoremus@gmail.com>
2022-07-22 23:08:24 -0400

    Dynamically discovers posts now

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -1,7 +1,27 @@
 /** @jsx h */
 import { h } from "preact";
-import { PageProps } from "$fresh/server.ts";
+import { Handlers, HandlerContext, PageProps } from "$fresh/server.ts";
+import { render } from "gfm"
 
-export default function Greet(props: PageProps) {
-  return <div>Hello {props.params.name}</div>;
+type GreetProps = {
+  name: string;
+}
+
+export const handler: Handlers = {
+  async GET(_: Request, ctx: HandlerContext) {
+    const name = ctx.params.name;
+    const contents = await Deno.readTextFile(`./posts/${name}.md`);
+    const baseUrl = Deno.env.get("IS_PROD") ? "https://cdoremus.deno.dev" : "https://localhost:8000";
+    const blog = render(contents, {baseUrl});
+    return ctx.render({blog});
+  }
+}
+
+export default function Greet({data}: PageProps) {
+    const { blog } = data;
+    return (
+      <div dangerouslySetInnerHTML={{__html: blog}}>
+        {blog}
+      </div>
+    )
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,19 +1,36 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
-import Counter from "../islands/Counter.tsx";
+import { HandlerContext, Handlers, PageProps } from "$fresh/server.ts";
 
-export default function Home() {
+export const handler: Handlers = {
+  async GET(_: Request, ctx: HandlerContext) {
+    const files: string[] = []
+    for await (const dirEntry of Deno.readDir("./posts")) {
+      const file = dirEntry.name;
+      files.push(file.split(".")[0]);
+    }
+    console.log("Files", files)
+    return await ctx.render({files});
+  }
+}
+
+export default function Home({data}: PageProps) {
+  const {files} = data;
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      <img
-        src="/logo.svg"
-        height="100px"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
-      <p>
-        <a href="/blog">Blog</a>
-      </p>
+      <h1>Posts</h1>
+      <div>
+        <ul class={tw`list-disc`}>
+          {
+            files.map((file:string ) => {
+              const linkText = file.replace("_", " ");
+              const capText = linkText[0].toLocaleUpperCase() + linkText.substring(1);
+              return (<li><a class={tw`text-xl`} href={`/${file}`}>{capText}</a></li>)
+            })
+          }
+        </ul>
+      </div>
     </div>
   );
 }
