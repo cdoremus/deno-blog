adc0723f3599501787ef8d2d95008f912525b2d2
Craig Doremus <cdoremus@gmail.com>
2022-08-26 22:47:27 -0400

    Fixed menu link

--- islands/MenuLink.tsx
+++ islands/MenuLink.tsx
@@ -5,7 +5,7 @@ import { tw } from "twind";
 
 export default function MenuLink() {
   const link = window.location?.href?.endsWith("/") ? "/about" : "/"
-  const linkText = window.location?.href?.endsWith("/about") ? "Home" : "About"
+  const linkText = window.location?.href?.endsWith("/") ? "About" : "Home"
   return (
     <div class={tw`text-right text-m`}>
       <a href={link}>{linkText}</a>
