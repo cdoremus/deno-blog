0c4c459fc8318cf80955f1088bcf1496791914bf
Craig Doremus <cdoremus@gmail.com>
2022-08-26 23:13:19 -0400

    Added middleware for cache header

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,18 +4,20 @@
 
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/_app.tsx";
-import * as $2 from "./routes/about.tsx";
-import * as $3 from "./routes/credits.tsx";
-import * as $4 from "./routes/index.tsx";
+import * as $2 from "./routes/_middleware.ts";
+import * as $3 from "./routes/about.tsx";
+import * as $4 from "./routes/credits.tsx";
+import * as $5 from "./routes/index.tsx";
 import * as $$0 from "./islands/MenuLink.tsx";
 
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
     "./routes/_app.tsx": $1,
-    "./routes/about.tsx": $2,
-    "./routes/credits.tsx": $3,
-    "./routes/index.tsx": $4,
+    "./routes/_middleware.ts": $2,
+    "./routes/about.tsx": $3,
+    "./routes/credits.tsx": $4,
+    "./routes/index.tsx": $5,
   },
   islands: {
     "./islands/MenuLink.tsx": $$0,
