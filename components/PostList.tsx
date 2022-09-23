657c2b1570377c6b55dadcd82712dce97eb6ae7e
Craig Doremus <cdoremus@gmail.com>
2022-09-23 11:55:40 -0400

    Added hover:underline to blog links

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -5,13 +5,13 @@ type PostListProps = {
 export default function PostList(props: PostListProps) {
   const { files } = props;
   return (
-    <ul class="list-disc ml-5 mr-5">
+    <ul class="list-disc ml-5 mr-5 group">
       {files.map((file: string) => {
         const postFile = file.split(".");
         const linkText = postFile[0].replaceAll("_", " ");
         return (
           <li>
-            <a class="text-xl" href={`/${postFile[0]}.${postFile[1]}`}>
+            <a class="text-xl hover:underline" href={`/${postFile[0]}.${postFile[1]}`}>
               {linkText} - {postFile[1]}
             </a>
           </li>
