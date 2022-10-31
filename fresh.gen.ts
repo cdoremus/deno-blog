b3c9610c5ccdf1c4c97b574bc4ca5854957b5046
Craig Doremus <cdoremus@gmail.com>
2022-10-31 14:43:55 -0400

    Revert "Remove [name].tsx to fix NotFoundError"
    This reverts commit 2c84f8a806a8526845cc16ee07bccbabb94ba43f.

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,22 +3,24 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/_404.tsx";
-import * as $1 from "./routes/_app.tsx";
-import * as $2 from "./routes/_middleware.ts";
-import * as $3 from "./routes/about.tsx";
-import * as $4 from "./routes/credits.tsx";
-import * as $5 from "./routes/index.tsx";
+import * as $0 from "./routes/[name].tsx";
+import * as $1 from "./routes/_404.tsx";
+import * as $2 from "./routes/_app.tsx";
+import * as $3 from "./routes/_middleware.ts";
+import * as $4 from "./routes/about.tsx";
+import * as $5 from "./routes/credits.tsx";
+import * as $6 from "./routes/index.tsx";
 import * as $$0 from "./islands/MenuLink.tsx";
 
 const manifest = {
   routes: {
-    "./routes/_404.tsx": $0,
-    "./routes/_app.tsx": $1,
-    "./routes/_middleware.ts": $2,
-    "./routes/about.tsx": $3,
-    "./routes/credits.tsx": $4,
-    "./routes/index.tsx": $5,
+    "./routes/[name].tsx": $0,
+    "./routes/_404.tsx": $1,
+    "./routes/_app.tsx": $2,
+    "./routes/_middleware.ts": $3,
+    "./routes/about.tsx": $4,
+    "./routes/credits.tsx": $5,
+    "./routes/index.tsx": $6,
   },
   islands: {
     "./islands/MenuLink.tsx": $$0,
