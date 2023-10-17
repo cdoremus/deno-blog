7bdbd14e7ca3e2fe986530d72a28b3608883cf4c
Craig Doremus <cdoremus@gmail.com>
2023-10-17 19:15:38 -0400

    Updated fn example test to use mock impl

--- tests/components/expect.test.tsx
+++ tests/components/expect.test.tsx
@@ -2,7 +2,7 @@ import { cleanup, render, setup } from "$fresh-testing-library/components.ts";
 import { expect, fn } from "$fresh-testing-library/expect.ts";
 import { afterEach, beforeAll, describe, it } from "std/testing/bdd.ts";
 
-describe("Experiments using the fresh-testing-library expect function...",  () => {
+describe("Experiments using the fresh-testing-library expect function...", () => {
   beforeAll(setup);
   afterEach(cleanup);
 
@@ -13,25 +13,27 @@ describe("Experiments using the fresh-testing-library expect function...",  () =
   it("should be truthy", () => {
     expect(1).toBeTruthy();
     expect(1).not.toBeFalsy();
-  })
+  });
 
   it("should be falsy", () => {
     expect(0).toBeFalsy();
     expect(0).not.toBeTruthy();
-  })
+  });
 
   it("should be empty element", () => {
-    const { getByRole } = render(<button></button>)
+    const { getByRole } = render(<button></button>);
     expect(getByRole("button")).toBeEmptyDOMElement();
   });
 
   it("should be required element", () => {
-    const { getByDisplayValue } = render(<input value="foobar" required></input>)
+    const { getByDisplayValue } = render(
+      <input value="foobar" required></input>,
+    );
     expect(getByDisplayValue("foobar")).toBeRequired();
   });
 
   it("should find text in document", () => {
-    const { queryByText } = render(<div>Hello World</div>)
+    const { queryByText } = render(<div>Hello World</div>);
     expect(queryByText("Hello World")).toBeInTheDocument();
     // using getByText will error out before matchers are called
     expect(queryByText("foobar")).not.toBeInTheDocument();
@@ -39,16 +41,14 @@ describe("Experiments using the fresh-testing-library expect function...",  () =
 
   it("should be able to use mock functions", () => {
     const add = (num1: number, num2: number): number => num1 + num2;
-    expect(add(2,6)).toBe(8);
-    // mock add and return value
-    const mockAdd = fn(add).mockReturnValue(5);
+    expect(add(2, 6)).toBe(8);
+    // create mock impl of add
+    const mockAdd = fn(add).mockImplementation((num1, num2) => num1 * num2);
     const sum = mockAdd(2, 2);
-    expect(sum).toBe(5);
+    expect(sum).toBe(4);
     expect(mockAdd).toBeCalled();
     expect(mockAdd).not.toBeCalledTimes(2);
-    expect(mockAdd(3,5)).toBe(5);
-    expect(mockAdd).toBeCalledWith(3,5)
+    expect(mockAdd(3, 5)).toBe(15);
+    expect(mockAdd).toBeCalledWith(3, 5);
   });
-
-
 });
