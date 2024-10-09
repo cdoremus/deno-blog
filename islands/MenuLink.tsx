d815f6702dc8c2a1c55ee0b94ba317a151d43ad7
Craig Doremus <cdoremus@gmail.com>
2024-10-09 13:21:33 -0400

    Fixes to run on Deno 2.0

--- islands/MenuLink.tsx
+++ islands/MenuLink.tsx
@@ -1,6 +1,6 @@
 export default function MenuLink() {
-  const link = window.location?.href?.endsWith("/") ? "/about" : "/";
-  const linkText = window.location?.href?.endsWith("/") ? "About" : "Home";
+  const link = globalThis.location?.href?.endsWith("/") ? "/about" : "/";
+  const linkText = globalThis.location?.href?.endsWith("/") ? "About" : "Home";
   return (
     <div class="text-right text-m">
       <a class="underline" href={link}>{linkText}</a>
